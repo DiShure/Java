5dce9a39e7e7f98fde06efbf20553ec5b344b778
Lorgus <facelsskyubei@gmail.com>
2024-11-07 19:57:10 +0300

    Подготовка граффики - ввод данных во время работы программы

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,64 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.lang.Integer.getInteger;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
+        int[] koords = new int[5];
+        JTextField smallField = new JTextField("300, 450, 25, 45, 20");
+        JTextField numbers = new JTextField("0, 1, 2, 3, 4, 5, 6, 7, 8, 9");
         JFrame f = new JFrame("Swing Paint Demo");
-
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
         MyPanel myPanel = new MyPanel();
 
+        numbers.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String[] parm = smallField.getText().replaceAll(" ","").split(",");
+                int[] koords = new int[parm.length];
+                if(parm.length != parm.length){
+                    JOptionPane.showMessageDialog(null, "Ошибка в колличестве праметров");
+                }
+                for (int i=0; i< parm.length; i++){
+                    koords[i] = getInteger(parm[i]);
+                }
+                myPanel.setX(koords[0]);
+                myPanel.setY(koords[1]);
+                myPanel.setWidth(koords[2]);
+                myPanel.setHeight(koords[3]);
+                myPanel.setStep(koords[4]);
+                myPanel.setSymbols(numbers.getText());
+                myPanel.repaint();
+                f.add(myPanel);
+                f.pack();
+                f.setVisible(true);
+            }
+        });
+
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel contents = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        contents.add(numbers);
+        contents.add(smallField);
+        f.setContentPane(contents);
+        //myPanel.setSymbols(numbers.getText());
+
+
         myPanel.repaint();
-
-        myPanel.setSymbols("1, 2, 3");
-
-
         f.add(myPanel);
-
         f.pack();
-
         f.setVisible(true);
+    }
+    private static int getInteger(String parm){
+        try {
+            return Integer.parseInt(parm);
+        }
+        catch (Exception e){
+            new Exception("Это не цифра");
+        }
+        return 0;
     }
 }
